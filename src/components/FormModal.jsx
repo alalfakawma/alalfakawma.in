36605a395ef4df89736f7ac6cadee7a0ecd4bed1
alalfakawma <alalfakawma@live.com>
2018-10-24 18:43:37 +0530

    Added form method

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -21,7 +21,7 @@ export default class FormModal extends React.Component {
 			<div ref={ this.modal } className={ !this.state.modalOpen ? "modal" : "modal is-active" }>
 				<div onClick={ this.toggleModal } className="modal-background"></div>
 				<div className="modal-content">
-					<form name="message-me" className="box" style={{ background: "#FFFDD0" }} netlify>
+					<form method="POST" name="message-me" className="box" style={{ background: "#FFFDD0" }} netlify>
 						<div className="field">
 						  <label className="label">Name</label>
 						  <div className="control">
