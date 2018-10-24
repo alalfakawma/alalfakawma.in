609fc8221fce71b2108892b320ec7c372ff70f85
alalfakawma <alalfakawma@live.com>
2018-10-24 17:11:39 +0530

    removed font-awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import '../node_modules/bulma/css/bulma.css';
-import '../node_modules/font-awesome/css/font-awesome.css';
 import './css/app.css';
 import MainContent from './components/MainContent.jsx';
 
