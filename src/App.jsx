66fb06cf87267b70538cae306a59ebda49d41c56
alalfakawma <alalfakawma@live.com>
2018-10-24 20:20:26 +0530

    remove troubleshooting form

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ class App extends Component {
     return (
       <Fragment>
         <MainContent />
-        <form netlify name="test-form">
-			<input type="text" name="name" />
-			<input type="submit" value="tell me ya name" />
-        </form>
       </Fragment>
     );
   }
