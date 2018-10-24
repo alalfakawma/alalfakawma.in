264f4faad3beed67690458333dec5c5cffccf02d
alalfakawma <alalfakawma@live.com>
2018-10-24 18:40:21 +0530

    Added form modal

---
+++ src/components/FormModal.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default class FormModal extends React.Component {
+	state = {
+		modalOpen: false
+	}
+
+	constructor(props) {
+		super(props);
+		this.modal = React.createRef();
+		this.toggleModal = this.toggleModal.bind(this);
+	}
+
+	toggleModal() {
+		if (this.state.modalOpen) this.setState({modalOpen: false})
+		else this.setState({modalOpen: true})
+	}
+
+	render() {
+		return (
+			<div ref={ this.modal } className={ !this.state.modalOpen ? "modal" : "modal is-active" }>
+				<div onClick={ this.toggleModal } className="modal-background"></div>
+				<div className="modal-content">
+					<form name="message-me" className="box" style={{ background: "#FFFDD0" }} netlify>
+						<div className="field">
+						  <label className="label">Name</label>
+						  <div className="control">
+						    <input className="input" type="text" name="name" />
+						  </div>
+						</div>
+
+						<div className="field">
+						  <label className="label">Email</label>
+						  <div className="control">
+						    <input className="input" type="email" name="email" />
+						  </div>
+						</div>
+
+						<div class="field">
+						  <label class="label">Message</label>
+						  <div class="control">
+						    <textarea class="textarea" name="message"></textarea>
+						  </div>
+						</div>
+
+						<div className="field has-text-centered">
+							<button type="submit" className="button send-message-button">Send Message</button>
+						</div>
+					</form>
+				</div>
+				<button onClick={ this.toggleModal } className="modal-close is-large" aria-label="close"></button>
+			</div>
+		);
+	}
+}
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import Typer from './Typer.jsx';
+import FormModal from './FormModal.jsx';
 
 export default class MainContent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.formModal = React.createRef();
+		this.toggleModal = this.toggleModal.bind(this);
+	}
+
+	toggleModal() {
+		this.formModal.current.toggleModal();
+	}
+
 	render() {
 		return (
-			<div className="content-holder is-size-4">
-				<span>&raquo; Hello, I am</span>
-				<div>
-					&raquo; <Typer text="Aseem Lalfakawma." textClass="has-text-weight-bold" />
+			<React.Fragment>
+				<FormModal ref={ this.formModal } />
+				<div className="content-holder is-size-4">
+					<span>&raquo; Hello, I am</span>
+					<div>
+						&raquo; <Typer text="Aseem Lalfakawma." textClass="has-text-weight-bold" />
+					</div>
+					<span>&raquo; I am a <span className="has-text-weight-bold">web developer</span> from Mizoram, India.</span>
+					<div>&raquo; <Typer typeSpeed={150} text="I make modern web apps using Vue, React and Laravel." /></div>
+					<div>
+						&raquo; <a href="https://freelancer.in/u/alalfakawma" target="_blank" rel="noopener noreferrer">
+							hire me
+						</a>
+						&nbsp;|&nbsp;
+						<a onClick={ this.toggleModal }>
+							message me
+						</a>
+					</div>
 				</div>
-				<span>&raquo; I am a <span className="has-text-weight-bold">web developer</span> from Mizoram, India.</span>
-				<div>&raquo; <Typer typeSpeed={150} text="I make modern web apps using Vue, React and Laravel." /></div>
-				<div>
-					&raquo; <a href="https://freelancer.in/u/alalfakawma" target="_blank" rel="noopener noreferrer">
-						hire me
-					</a>
-					&nbsp;|&nbsp;
-					<a>
-						message me
-					</a>
-				</div>
-			</div>
+			</React.Fragment>
 		);
 	}
 }
